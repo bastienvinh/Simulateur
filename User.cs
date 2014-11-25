00d41133c7e9b503ab0ae0debbda17c2bc0de7b9
lasaad <lasaad.benzaied@gmail.com>
2014-11-25 14:55:21 +0100

    ajout des classes user et bankbook

---
+++ User.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace Simulateur.Data
+{
+    class User
+    {
+        private uint initPay; //down payment
+        private uint monthPay; //monthly payment
+        private uint duration; //saving duration (by Month)
+        private uint total; //total
+
+        public uint INITPAY
+        {
+            get
+            {
+                return initPay;
+            }
+            set
+            {
+                initPay = value;
+
+            }
+        }
+        public uint MONTHPAY
+        {
+            get
+            {
+                return monthPay;
+            }
+            set
+            {
+                monthPay = value;
+
+            }
+        }
+        public uint DURATION
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                duration = value;
+
+            }
+        }
+        public uint TOTAL
+        {
+            get
+            {
+                return INITPAY + (MONTHPAY * DURATION);
+            }
+            set
+            {
+                total = value;
+
+            }
+        }
+    };
+}
