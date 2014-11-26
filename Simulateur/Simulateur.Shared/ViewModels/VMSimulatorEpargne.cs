13db34ecd308afc220eca64bc0b59824847007fc
kodoma <kodomakodoma@yopmail.com>
2014-11-26 01:16:07 +0100

    Modification About Window (Page)

---
+++ Simulateur/Simulateur.Shared/ViewModels/VMSimulatorEpargne.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Simulateur.Common;
+
+namespace Simulateur.ViewModels
+{
+	public class VMSimulatorEpargne : ViewModelBase
+	{
+
+
+	}
+}
---
+++ Simulateur/Simulateur.Shared/ViewModels/VMSimulatorHelp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.ApplicationModel.Resources;
+using Simulateur.Common;
+
+namespace Simulateur.ViewModels
+{
+	public class VMSimulatorHelp : ViewModelBase
+	{
+
+		#region Constants
+
+		private const string FILE_RESOURCE = "Resources";
+
+		#endregion
+
+		#region Properties
+
+		private readonly string _textExplication;
+
+		#endregion
+
+
+		#region
+
+		public string TextExplication { get { return _textExplication; } }
+
+		#endregion
+
+		#region Constructor
+
+		public VMSimulatorHelp()
+		{
+
+			ResourceLoader resourceLanguage = ResourceLoader.GetForCurrentView(FILE_RESOURCE);
+			_textExplication = resourceLanguage.GetString("AboutMessage");
+		}
+
+		#endregion
+
+	}
+}
