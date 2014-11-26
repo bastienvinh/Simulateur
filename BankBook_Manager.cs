73a35a6c18a82ac212f35209cf15641d241379ae
lasaad <lasaad.benzaied@gmail.com>
2014-11-26 22:15:44 +0100

    ajout Json, bank_manager et thriftUtils

---
+++ BankBook_Manager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Simulateur.Data;
+
+public class BankBook_Manager
+{
+    public BankBook_Manager();
+     static void load_all_data()
+        {
+
+            BankBook livretA = new BankBook(_rate = ,
+         _time = ,
+         _minRate = ,
+         _ceiling = ,
+         _paymentMonth = ,
+         _taxSubject = );
+                
+                
+                );
+
+
+
+
+        }
+
+}
+
---
+++ Json.js
@@ -0,0 +1,66 @@
+// JavaScript source code
+{
+
+    "Livret" : [
+        {
+            "nom" : " AXA banque",
+            "rate" : "4",
+            "ceiling" :"",
+            "holder" : "" ,
+            "manaFees" : "0",
+            "time": "0",
+            "access":"0",
+            "taxSubject":"1"
+        },     
+        {
+            "nom" : "Développement Durable",
+            "rate" : "1",
+            "ceiling" :"12000",
+            "holder" : "1" ,
+            "manaFees" :"1",
+            "time": "",
+            "access":"1",
+            "taxSubject":"1"
+        },
+        {
+            "nom" : "Jeune",
+            "rate" : "2.75",
+            "ceiling" :"1600",
+            "holder" : "1" ,
+            "manaFees" : "0",
+            "time": "0",
+            "access":"0",
+            "taxSubject":"1"
+        },
+        {
+            "nom" : "Plan Epargne Logement",
+            "rate" : "2.50",
+            "ceiling" :"61200",
+            "holder" : "" ,
+            "manaFees" : "0",
+            "time": "0",
+            "access":"0",
+            "taxSubject":"1"
+        },
+        {
+            "nom" : "CEL",
+            "rate" : "0.75",
+            "ceiling" :"15300",
+            "holder" : "" ,
+            "manaFees" : "0",
+            "time": "0",
+            "access":"0",
+            "taxSubject":"1"
+        },
+        {
+            "nom" : " AXA banque",
+            "rate" : "4",
+            "ceiling" :"",
+            "holder" : "" ,
+            "manaFees" : "0",
+            "time": "0",
+            "access":"0",
+            "taxSubject":"1"
+        },
+    ]
+}
