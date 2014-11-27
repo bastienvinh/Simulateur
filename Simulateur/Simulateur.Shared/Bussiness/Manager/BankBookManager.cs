899bfab96652894b23b9b9dc07505fba7738a51b
kodoma <kodomakodoma@yopmail.com>
2014-11-27 15:44:59 +0100

    Modification Struc Code

---
+++ Simulateur/Simulateur.Shared/Bussiness/Manager/BankBookManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Simulateur.Data;
+
+namespace Simulateur.Bussiness
+{
+	public static class BankBookManager
+	{
+		private static List<BankBook> __bankBooks;
+
+		public static List<BankBook> BankBooks
+		{
+			get { return __bankBooks; }
+		}
+
+		public static void LoadAllDatas()
+		{
+			// TODO ; Complete this
+			// fill __bankbook
+		}
+
+
+		public static void LoadAllDatas(string jsonFile)
+		{
+			
+			
+
+		}
+
+	}
+}
