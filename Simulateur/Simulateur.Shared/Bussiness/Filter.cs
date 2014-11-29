f36e8aa46c42cea82bf5ef3b7ef09c7c4add8f80
kodoma <kodomakodoma@yopmail.com>
2014-11-29 22:56:01 +0100

    Ajout de la class filter et modification de BankbookManager

---
+++ Simulateur/Simulateur.Shared/Bussiness/Filter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulateur.Bussiness
+{
+	public class Filter
+	{
+		//public uint Graduation { get; set; }
+		public bool IsAStudent { get; set; }
+		public uint NbChilds { get; set; }
+		public uint Capital { get; set; } 
+		public uint Duration { get; set; }
+		public uint MonthPay { get; set; } 
+		public uint Deposit { get; set; }
+
+		public Filter()
+		{
+			
+		}
+	}
+}
--- Simulateur/Simulateur.Shared/Bussiness/Manager/BankBookManager.cs
+++ Simulateur/Simulateur.Shared/Bussiness/Manager/BankBookManager.cs
@@ -13,17 +13,49 @@ namespace Simulateur.Bussiness
 {
 	public static class BankBookManager
 	{
-		private static List<BankBook> __bankBooks;
 
+		#region Constantes
+		private const decimal MAX_RATE_YOUNG = 1;
+		#endregion
+
+		#region Static Attributes
+		private static List<BankBook> __bankBooks;
+		#endregion
+
+		#region Properties
 		public static List<BankBook> BankBooks
 		{
 			get { return __bankBooks; }
 		}
+		#endregion
 
+
+		#region Operations
 		public async static Task LoadAllDatasAsync()
 		{
 			__bankBooks = await BLLBankBooks.GetAllAsync();
 		}
+
+		public static List<BankBook> GetListByFilter(Filter filter)
+		{
+			List<BankBook> res = new List<BankBook>();
+			if (__bankBooks != null)
+			{
+				foreach (BankBook b in __bankBooks)
+				{
+					// Filtre
+					if (b.Rate <= MAX_RATE_YOUNG)
+					{
+						res.Add(b);
+					}
+
+					// TODO : finir le filtre
+				}
+
+			}
+			return res;
+		}
+		#endregion
 
 	}
 }
